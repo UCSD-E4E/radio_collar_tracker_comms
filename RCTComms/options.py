984d4138cbd39956e718b2c37d84862541b835e5
Nathan Hui <nthui@eng.ucsd.edu>
2023-07-19 20:00:49 -0700

    fix: Updates option parameters

--- RCTComms/options.py
+++ RCTComms/options.py
@@ -3,11 +3,12 @@
 import struct
 from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Callable, Optional, Type, Tuple, Sequence
+from typing import Any, Callable, Optional, Type, Tuple, TypeVar
 
 BASE_OPTIONS = 0x01
 EXP_OPTIONS = 0x02
 ENG_OPTIONS = 0x04
+ALL_OPTIONS = 0xFF
 
 class Options(Enum):
     DSP_PING_WIDTH = 'DSP_ping_width'
@@ -28,16 +29,21 @@ class Options(Enum):
     SYS_WIFI_MONITOR_INTERVAL = 'SYS_wifi_monitor_interval'
     SYS_HEARTBEAT_PERIOD = 'SYS_heartbeat_period'
 
+T = TypeVar('T')
 @dataclass
 class OptionParams:
-    type_list: Sequence[Type]
+    """Option Parameter definitions
+
+    T must match format_str
+    """
+    type_list: Tuple[Type[T]]
+    default_value: Any
     format_str: str
     length: int
     scope_mask: int
-    validation_fn: Optional[Callable[[Any], bool]] = None
-    transform_fn: Optional[Callable[[Any], Any]] = None
+    validation_fn: Optional[Callable[[T], bool]] = None
 
-    def unpack_from(self, buffer: bytes, offset: int) -> Tuple[Any, int]:
+    def unpack_from(self, buffer: bytes, offset: int) -> Tuple[T, int]:
         """Unpacks this option from the buffer at the specified offset
 
         Args:
@@ -45,18 +51,16 @@ class OptionParams:
             idx (int): Offset to unpack from
 
         Returns:
-            Tuple[Any, int]: Value, and index of next byte
+            Tuple[U, int]: Value, and index of next byte
         """
         if self.format_str == 's':
             strlen, = struct.unpack_from('<H', buffer, offset=offset)
             new_str = buffer[offset + 2:offset + 2 + strlen].decode('ascii')
             return new_str, offset + 2 + strlen
         value, = struct.unpack_from(self.format_str, buffer, offset=offset)
-        if self.transform_fn:
-            value = self.transform_fn(value)
         return value, offset + self.length
 
-    def pack(self, value: Any) -> bytes:
+    def pack(self, value: T) -> bytes:
         """Packs this option into a buffer
 
         Args:
@@ -66,61 +70,67 @@ class OptionParams:
             bytes: Binary blob
         """
         assert isinstance(value, self.type_list)
+        if self.validation_fn:
+            if not self.validation_fn(value):
+                raise ValueError('Failed validation')
         if self.format_str == 's':
             assert isinstance(value, str)
             strlen = len(value)
             return struct.pack(f'<H{strlen}s', strlen, value.encode('ascii'))
         return struct.pack(self.format_str, value)
-            
+
 
 option_param_table = {
     Options.DSP_PING_WIDTH: OptionParams(
-        type_list=(int, float),
+        type_list=(float),
         format_str='<f',
+        default_value=27.,
         length=4,
         scope_mask=EXP_OPTIONS,
         validation_fn=lambda x: x > 0,
-        transform_fn=float
     ),
     Options.DSP_PING_SNR: OptionParams(
-        type_list=(int, float),
+        type_list=(float),
         format_str='<f',
+        default_value=0.1,
         length=4,
         scope_mask=EXP_OPTIONS,
         validation_fn=lambda x: x > 0,
-        transform_fn=float
     ),
     Options.DSP_PING_MAX: OptionParams(
-        type_list=(int, float),
+        type_list=(float),
         format_str='<f',
+        default_value=1.5,
         length=4,
         scope_mask=EXP_OPTIONS,
         validation_fn=lambda x: x > 1,
-        transform_fn=float
     ),
     Options.DSP_PING_MIN: OptionParams(
-        type_list=(int, float),
+        type_list=(float),
         format_str='<f',
+        default_value=0.5,
         length=4,
         scope_mask=EXP_OPTIONS,
         validation_fn=lambda x: 0 < x < 1,
-        transform_fn=float
     ),
     Options.GPS_MODE: OptionParams(
         type_list=(bool),
         format_str='<?',
+        default_value=False,
         length=1,
         scope_mask=ENG_OPTIONS
     ),
     Options.GPS_DEVICE: OptionParams(
         type_list=(str),
         format_str='s',
+        default_value='/dev/null',
         length=2,
         scope_mask=ENG_OPTIONS
     ),
     Options.GPS_BAUD: OptionParams(
         type_list=(int),
         format_str='<L',
+        default_value=9600,
         length=4,
         scope_mask=ENG_OPTIONS,
         validation_fn=lambda x: x > 0
@@ -128,24 +138,28 @@ option_param_table = {
     Options.TGT_FREQUENCIES: OptionParams(
         type_list=(list),
         format_str='',
+        default_value=[],
         length=4,
         scope_mask=0xFF
     ),
     Options.SYS_AUTOSTART: OptionParams(
         type_list=(bool),
         format_str='<?',
+        default_value=False,
         length=1,
         scope_mask=ENG_OPTIONS,
     ),
     Options.SYS_OUTPUT_DIR: OptionParams(
         type_list=(str),
         format_str='s',
+        default_value='../testOutput',
         length=2,
         scope_mask=EXP_OPTIONS,
     ),
     Options.SDR_SAMPLING_FREQ: OptionParams(
         type_list=(int),
         format_str='<L',
+        default_value=1500000,
         length=4,
         scope_mask=BASE_OPTIONS,
         validation_fn=lambda x: x > 0
@@ -153,32 +167,36 @@ option_param_table = {
     Options.SDR_CENTER_FREQ: OptionParams(
         type_list=(int),
         format_str='<L',
+        default_value=173500000,
         length=4,
         scope_mask=BASE_OPTIONS,
         validation_fn=lambda x: x > 0
     ),
     Options.SDR_GAIN: OptionParams(
-        type_list=(int, float),
+        type_list=(float),
         format_str='<f',
+        default_value=20.0,
         length=4,
         scope_mask=BASE_OPTIONS,
-        transform_fn=float
     ),
     Options.SYS_NETWORK: OptionParams(
         type_list=(str),
         format_str='s',
+        default_value='ubnt',
         length=2,
         scope_mask=EXP_OPTIONS,
     ),
     Options.SYS_WIFI_MONITOR_INTERVAL: OptionParams(
         type_list=(int),
         format_str='<L',
+        default_value=15,
         length=4,
         scope_mask=EXP_OPTIONS
     ),
     Options.SYS_HEARTBEAT_PERIOD: OptionParams(
         type_list=(int),
         format_str='<L',
+        default_value=5,
         length=4,
         scope_mask=EXP_OPTIONS,
         validation_fn=lambda x: x > 0
@@ -186,6 +204,7 @@ option_param_table = {
     Options.GCS_SPEC: OptionParams(
         type_list=(str),
         format_str='s',
+        default_value='tcpc://192.168.1.1:9600',
         length=2,
         scope_mask=ENG_OPTIONS,
     )
